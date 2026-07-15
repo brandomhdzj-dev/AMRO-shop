9163afb73fb23bd061629275b1af7cb5a9cc2969
Brandom Hernández Jacobo <brandomhdzj@gmail.com>
2026-07-14 21:22:16 -0600

    Fix: Cloudinary using Spring Environment + debug log

--- src/main/java/com/brandom/mipaginaweb/config/CloudinaryConfig.java
+++ src/main/java/com/brandom/mipaginaweb/config/CloudinaryConfig.java
@@ -1,26 +1,27 @@
 package com.brandom.mipaginaweb.config;
 
 import com.cloudinary.Cloudinary;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${CLOUDINARY_CLOUD_NAME:}")
-    private String cloudName;
-
-    @Value("${CLOUDINARY_API_KEY:}")
-    private String apiKey;
-
-    @Value("${CLOUDINARY_API_SECRET:}")
-    private String apiSecret;
+    @Autowired
+    private Environment env;
 
     @Bean
     public Cloudinary cloudinary() {
+        String cloudName = env.getProperty("CLOUDINARY_CLOUD_NAME", "");
+        String apiKey = env.getProperty("CLOUDINARY_API_KEY", "");
+        String apiSecret = env.getProperty("CLOUDINARY_API_SECRET", "");
+
+        System.out.println("CLOUDINARY DEBUG: cloud_name=[" + cloudName + "] api_key=[" + apiKey + "] api_secret=[" + apiSecret + "]");
+
         return new Cloudinary(Map.of(
             "cloud_name", cloudName,
             "api_key", apiKey,
